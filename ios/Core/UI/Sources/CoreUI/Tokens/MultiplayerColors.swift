4172da873786d75c192b34f18ed77ac8ea2c9a57
alexander <shirokih.a9@rwb.ru>
2026-03-28 14:46:45 +0300

    [ios] feat: расширить токены цветовой системы

--- ios/Core/UI/Sources/CoreUI/Tokens/MultiplayerColors.swift
+++ ios/Core/UI/Sources/CoreUI/Tokens/MultiplayerColors.swift
@@ -9,8 +9,15 @@ public struct MultiplayerColors {
     public let surfaceSecondary: Color
     public let surfaceOverlay: Color
     public let surfaceAccent: Color
+    public let surface2GradientStart: Color
+    public let surface2GradientEnd: Color
+    public let surface3GradientStart: Color
+    public let surface3GradientEnd: Color
+    public let surface4GradientStart: Color
+    public let surface4GradientEnd: Color
     public let textPrimary: Color
     public let textSecondary: Color
+    public let textTertiary: Color
     public let textInverse: Color
     public let accent: Color
     public let accentMuted: Color
@@ -19,6 +26,8 @@ public struct MultiplayerColors {
     public let brandVisualPrimaryContainer: Color
     public let brandVisualSecondary: Color
     public let brandVisualSecondaryMuted: Color
+    public let brandVisualTertiary: Color
+    public let brandVisualTertiaryMuted: Color
     public let ctaGradientStart: Color
     public let ctaGradientEnd: Color
     public let borderSubtle: Color
@@ -30,6 +39,30 @@ public struct MultiplayerColors {
     public var backgroundGradient: LinearGradient {
         LinearGradient(
             colors: [backgroundGradientStart, backgroundGradientEnd],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+
+    public var surface2Gradient: LinearGradient {
+        LinearGradient(
+            colors: [surface2GradientStart, surface2GradientEnd],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+
+    public var surface3Gradient: LinearGradient {
+        LinearGradient(
+            colors: [surface3GradientStart, surface3GradientEnd],
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+
+    public var surface4Gradient: LinearGradient {
+        LinearGradient(
+            colors: [surface4GradientStart, surface4GradientEnd],
             startPoint: .topLeading,
             endPoint: .bottomTrailing
         )
